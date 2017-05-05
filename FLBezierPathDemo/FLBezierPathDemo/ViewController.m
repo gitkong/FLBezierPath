96c8a35f2d94f39980b53127de4f461a894880af
gitKong <fanlie.kong@qlchat.com>
2017-05-05 16:45:34 +0800

    绘制箭头

--- FLBezierPathDemo/FLBezierPathDemo/ViewController.m
+++ FLBezierPathDemo/FLBezierPathDemo/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "FLBezierView.h"
+#import "UIBezierPath+FLUnits.h"
 @interface ViewController ()
 
 @end
@@ -17,11 +18,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    FLBezierView *beView = [[FLBezierView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
-    beView.backgroundColor = [UIColor lightTextColor];
-    [self.view addSubview:beView];
+//    FLBezierView *beView = [[FLBezierView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+//    beView.backgroundColor = [UIColor lightTextColor];
+//    [self.view addSubview:beView];
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    UIView *alertView = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 200, 30)];
+    [self drawBackViewWithView:alertView cornerRadius:5 triangleCenterX:150 triangleSize:CGSizeMake(10, 10) backColor:[UIColor whiteColor] text:@"hello gitKong"];
+    [self.view addSubview:alertView];
+    
 }
 
+- (void)drawBackViewWithView:(UIView *)view cornerRadius:(CGFloat)cornerRadius triangleCenterX:(CGFloat)triangleCenterX triangleSize:(CGSize)triangleSize backColor:(UIColor *) color text:(NSString *)string{
+    CGSize finalSize = CGSizeMake(CGRectGetWidth(view.bounds), CGRectGetHeight(view.bounds));
+    CAShapeLayer *layer = [CAShapeLayer layer];
+    
+    layer.path = UIBezierPath.fl_path.maker
+    .moveTo(cornerRadius, 0)
+    .addArcWith(CGPointMake(cornerRadius, cornerRadius),5,0,180,NO)
+    .addLineTo(0, finalSize.height - cornerRadius)
+    .addArcWith(CGPointMake(cornerRadius, finalSize.height - cornerRadius),5,180,90,NO)
+    .addLineTo(finalSize.width - cornerRadius, finalSize.height)
+    .addArcWith(CGPointMake(finalSize.width - cornerRadius, finalSize.height - cornerRadius),5,270,0,NO)
+    .addLineTo(finalSize.width, cornerRadius)
+    .addArcWith(CGPointMake(finalSize.width - cornerRadius, cornerRadius),5,360,-90,NO)
+    .addLineTo(triangleCenterX + triangleSize.width / 2,0)
+    .addLineTo(triangleCenterX,-triangleSize.height)
+    .addLineTo(triangleCenterX - triangleSize.width / 2,0)
+    .addLineTo(0, 0)
+    .lineJoinStyle(kCGLineJoinRound)
+    .fill().CGPath;
+    
+    layer.fillColor = color.CGColor;
+    [view.layer addSublayer:layer];
+    UILabel *label = [[UILabel alloc] initWithFrame:view.bounds];
+    label.text = string;
+    label.textColor = [UIColor blackColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont systemFontOfSize:15];
+    [view addSubview:label];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
